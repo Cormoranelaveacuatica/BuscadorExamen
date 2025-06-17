a75209e9caaa7128c245b4eb1d12249412a71000
Jaime <jaim>
2025-06-17 10:22:48 +0200

    Initial Commit

---
+++ BuscadorExamenTests/BuscadorExamenTests.swift
@@ -0,0 +1,17 @@
+//
+//  BuscadorExamenTests.swift
+//  BuscadorExamenTests
+//
+//  Created by Mañanas on 17/6/25.
+//
+
+import Testing
+@testable import BuscadorExamen
+
+struct BuscadorExamenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
